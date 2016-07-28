6b2c61c6f2ec83efcc9c9e5ce87ef64d76de87df
Ross Cooper <therossjcooper@gmail.com>
2016-07-28 01:13:10 +0100

    Added whitelist plugin and updated app config

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,29 +15,29 @@ angular.module('starter.controllers', [])
 
   //Initialise the map and add a marker to current position
   var options = {timeout: 10000, enableHighAccuracy: true};
- 
+  
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
+   
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+    
     var mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+    
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
- 
-  var marker = new google.maps.Marker({
-      map: $scope.map,
-      animation: google.maps.Animation.DROP,
-      position: latLng
-  });      
- 
-});
- 
+     
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });      
+      
+    });
+    
   }, function(error){
     console.log("Could not get location");
   });
