77d018dc847604b5ba2d41900351cc4e7718d6fc
Ross Cooper <ross.cooper@teknet.org.uk>
2016-07-28 14:08:32 +0100

    Map and location now working

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,49 +1,73 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $ionicPlatform) {
 
-  $scope.clocked_in = false;
-  $scope.time = 0;
+  $ionicPlatform.ready(function() {  
 
-  $scope.clockIn = function(){
-    $scope.clocked_in = true;
-  }
-
-  $scope.clockOut = function(){
     $scope.clocked_in = false;
-  }
+    $scope.time = 0;
+    $scope.latLng = new google.maps.LatLng(0, 0);
+    $scope.history = [];
+
+    $scope.clockIn = function(){
+      $scope.clocked_in = true;
+    }
+
+    $scope.clockOut = function(){
+      $scope.clocked_in = false;
+    }
 
   //Initialise the map and add a marker to current position
   var options = {timeout: 10000, enableHighAccuracy: true};
-  
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-   
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-     
-      var marker = new google.maps.Marker({
-        map: $scope.map,
-        animation: google.maps.Animation.DROP,
-        position: latLng
-      });      
-      
-    });
-    
+  var mapOptions = {
+    center: $scope.latLng,
+    zoom: 1,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
+  google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+
+   $cordovaGeolocation.getCurrentPosition(options).then(function(position){ 
+
+    $scope.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var marker = new google.maps.Marker({
+      map: $scope.map,
+      animation: google.maps.Animation.DROP,
+      position: $scope.latLng
+    });   
+
+    $scope.map.setCenter($scope.latLng);
+    $scope.map.setZoom(18);   
+
   }, function(error){
-    console.log("Could not get location");
-  });
+   $scope.showAlert('Cannot get location');
+
+ },
+ { timeout: 15000 });
+ });
+
+  // An alert dialog
+  $scope.showAlert = function(error) {
+   var alertPopup = $ionicPopup.alert({
+     title: 'Error',
+     template: error
+   });
+
+   alertPopup.then(function(res) {
+     console.log('Thank you for not eating my delicious ice cream cone');
+   });
+ };
+
+});
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
+  .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -58,12 +82,12 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
