4a1a8a311bb3e7cf907d833145de3d2edd0f0a05
Ross Cooper <ross.cooper@teknet.org.uk>
2016-07-28 17:11:13 +0100

    Logs now saving and time is formatted using moment.js

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services', 'angularMoment', 'ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -50,13 +50,14 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.history', {
+      url: '/history',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
+        'tab-history': {
+          templateUrl: 'templates/tab-history.html',
+          controller: 'HistoryCtrl'
+        },
+        reload : true
       }
     })
     .state('tab.chat-detail', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,93 +1,111 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $ionicPlatform) {
+.controller('MapCtrl', function($scope, $state, $cordovaGeolocation, $ionicPopup, $ionicPlatform,  $localStorage) {
 
   $ionicPlatform.ready(function() {  
 
-    $scope.clocked_in = false;
-    $scope.time = 0;
-    $scope.latLng = new google.maps.LatLng(0, 0);
-    $scope.history = [];
+//Setup our scope variables
+$scope.$storage = $localStorage;
+$scope.position = { lat: 0, lng : 0};
+$scope.logs = $localStorage.logs || [];
+$scope.log = $localStorage.logs[0] || {};
 
+    //Performs the clock in function
     $scope.clockIn = function(){
-      $scope.clocked_in = true;
+      $scope.log.start = {
+        lat : $scope.position.lat,
+        lng : $scope.position.lng,
+        time : new Date(),
+      }; 
+      $scope.logs.push($scope.log); 
+      $localStorage.logs = $scope.logs;    
     }
 
+    //Clocks out current log and saves it to localStorage
     $scope.clockOut = function(){
-      $scope.clocked_in = false;
+      $scope.log.end = {
+        lat : $scope.position.lat,
+        lng : $scope.position.lng,
+        time : new Date(),
+      };
+      $localStorage.logs = $scope.logs;
+      $scope.log = {};
+
     }
+
+    // Show alert dialog
+    $scope.showAlert = function(error) {
+     var alertPopup = $ionicPopup.alert({
+       title: 'Error',
+       template: error
+     });
+   };
+
+   $scope.deleteLog = function(index){
+    if($scope.logs[index] == $scope.log){
+      $scope.log = {};
+    }
+    $scope.logs.splice(index, 1);
+  }
 
   //Initialise the map and add a marker to current position
   var options = {timeout: 10000, enableHighAccuracy: true};
 
-  var mapOptions = {
-    center: $scope.latLng,
-    zoom: 1,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+//Default map options
+var mapOptions = {
+  center: new google.maps.LatLng($scope.position),
+  zoom: 1,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
 
-  $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//Build the map
+$scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
+//Once map is idle and ready
+google.maps.event.addListenerOnce($scope.map, 'idle', function(){
 
-  google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+//Get the users location
+$cordovaGeolocation.getCurrentPosition(options).then(function(position){ 
 
-   $cordovaGeolocation.getCurrentPosition(options).then(function(position){ 
+  //Set these coordinates to scope variables
+  $scope.position.lat = position.coords.latitude;
+  $scope.position.lng = position.coords.longitude;
 
-    $scope.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //Make a new map latLng object with the users coords
+  var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    var marker = new google.maps.Marker({
-      map: $scope.map,
-      animation: google.maps.Animation.DROP,
-      position: $scope.latLng
-    });   
+//Make a map marker using users coords
+var marker = new google.maps.Marker({
+  map: $scope.map,
+  animation: google.maps.Animation.DROP,
+  position: latLng
+});   
 
-    $scope.map.setCenter($scope.latLng);
-    $scope.map.setZoom(18);   
+//Center and zoom in on the marker
+$scope.map.setCenter(latLng);
+$scope.map.setZoom(18);  
 
-  }, function(error){
-   $scope.showAlert('Cannot get location');
+}, function(error){
+  //Tell the user we cannot get their location
+  $scope.showAlert('Cannot get location');
+},{ timeout: 15000 });
 
- },
- { timeout: 15000 });
- });
-
-  // An alert dialog
-  $scope.showAlert = function(error) {
-   var alertPopup = $ionicPopup.alert({
-     title: 'Error',
-     template: error
-   });
-
-   alertPopup.then(function(res) {
-     console.log('Thank you for not eating my delicious ice cream cone');
-   });
- };
-
+});
 });
 
 })
 
-  .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('HistoryCtrl', function($scope, $localStorage) {
+  //Get the logs from the localStorage
+  $scope.logs = $localStorage.logs || [];
 })
 
-  .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-    $scope.chat = Chats.get($stateParams.chatId);
-  })
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
 
-  .controller('AccountCtrl', function($scope) {
-    $scope.settings = {
-      enableFriends: true
-    };
-  });
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
